bff0ae8e7252de601a7fc1d24dae0ebf60316811
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-06-09 22:15:13 +0530

    Whyred : Cherish~ifY !

--- cherish_whyred.mk
+++ cherish_whyred.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CherishOS stuff
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,10 +28,23 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 # Inherit from custom vendor.
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
+# Cherish Properties
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := true
+CHERISH_BUILD_TYPE := OFFICIAL
+
+# Maintainer Props
+PRODUCT_GENERIC_PROPERTIES += \
+    ro.cherish.maintainer=P.M.SALMAN_KHAN
+
+# Device Specfic
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := cherish_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
